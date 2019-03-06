7829ef2d19e959fadf028597c6cc5e0473680a49
ziji <ziji@DESKTOP-QSO21IB>
2019-03-06 22:42:43 +0800

    添加项目文件。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class HelloWorld
+    {
+        private static string strHelloWorld;
+
+        static HelloWorld()
+        {
+            strHelloWorld = "Hello World";
+        }
+        void writeHelloWorld()
+        {
+            Console.WriteLine(strHelloWorld);
+        }
+
+        public static void Main()
+        {
+            HelloWorld hw = new HelloWorld();
+            hw.writeHelloWorld();
+        }
+    }
+}
